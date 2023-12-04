7f23448077a9e8b6e3d55f805b067e188dddaa9c
Eric Hile <125088343+ehile9@users.noreply.github.com>
2023-12-04 21:04:52 +0000

    added style to slick-demo

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $('.fade').slick({
       dots: true,
       infinite: true,
-      speed: 500,
+      speed: 200,
       fade: true,
       slide: 'div',
       cssEase: 'linear',
